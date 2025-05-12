0a21b846d71bd7e43f03b8abc47b00b32b0faf42
SamueleFerraro <ellsamueleferraro@gmail.com>
2025-05-12 11:45:41 +0200

    Homework-9
    Project+report+README:
    Led matrix, SPI

--- Homework-9/Homework9_LED_MAT/Core/Src/main.c
+++ Homework-9/Homework9_LED_MAT/Core/Src/main.c
@@ -14,9 +14,9 @@
   * If no LICENSE file comes with this software, it is provided AS-IS.
   *
   ******************************************************************************
-  * GOAL:
-  * Read the acceleration measured by the accelerometer and send it to a remote
-  * terminal every 1 second. Using timer interrupts, I2C DMA and UART DMA.
+  *GOAL:
+  *Transmit a letter to the LED matrix using SPI and a timer interrupt & Alternate
+  *Transmit between two letters (or one letter and one symbol)
   */
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
@@ -24,7 +24,7 @@
 
 /* Private includes ----------------------------------------------------------*/
 /* USER CODE BEGIN Includes */
-#include "string.h"
+
 /* USER CODE END Includes */
 
 /* Private typedef -----------------------------------------------------------*/
@@ -34,9 +34,20 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-#define DATA_IN_DIM 5
-#define STR_LEN 32
-#define NUM_COOR 3
+
+/*Every char can be represented as 5 couples of value: one value regards the
+ * number of led that have to be turned on for each colon (in binary)
+ * while the other one is about which column (16,8,4,2,1)*/
+#define MATRIX_COL 2
+#define MATRIX_ROW 5
+
+/*The number of letters which I alternate on the led matrix is 2*/
+#define NUM_OF_CHAR 2
+
+/*RCLK is connected to the SPI pin SS (page 3 of hands-on lab schematics)
+ * RCLK is the Storage Clock, which turns on the parallel output registers*/
+#define RCLK GPIOB, GPIO_PIN_6
+
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -45,38 +56,38 @@
 /* USER CODE END PM */
 
 /* Private variables ---------------------------------------------------------*/
-I2C_HandleTypeDef hi2c1;
-DMA_HandleTypeDef hdma_i2c1_rx;
+SPI_HandleTypeDef hspi1;
+DMA_HandleTypeDef hdma_spi1_tx;
 
 TIM_HandleTypeDef htim2;
+TIM_HandleTypeDef htim3;
 
 UART_HandleTypeDef huart2;
-DMA_HandleTypeDef hdma_usart2_tx;
 
 /* USER CODE BEGIN PV */
-uint8_t slave_add1  = 0b01010000; //The correct address of my board (tested in an other project)
 
-// uint8_t slave_add12 = 0b0011000; is the other possible one for other boards
+//3D array to store all the char (matrix) we have to send
+uint8_t tx_char[NUM_OF_CHAR][MATRIX_ROW][MATRIX_COL] = {{
+			{49, 16}, //Letter S
+			{73, 8},
+			{73, 4},
+			{73, 2},
+			{70, 1},
+	},{
+			{64, 16}, // Lettere T
+			{64, 8},
+			{127, 4},
+			{64, 2},
+			{64, 1}
+	}
+};
 
-// register address of the accelerometer. MSB = 1 for the autoincrement (PAG 22 Datasheet)
-uint8_t reg_add_x = 0b10101001;
+/*Index to decide which letter has to be transmitted*/
+uint8_t letter_index = 0;
 
-/*ctrl_reg1[0] = address of the internal CTRL_REG1, ctrl_reg1[1]: Configuration meant for my application:
- * 0001 =  1 Hz refresh rate of the Accelerometer, 0111 = reading X,Y and Z coordinates*/
-uint8_t ctrl_reg1[2] = {0x20 , 0b00010111};
+/*Index to decide which column I turn ON*/
+uint8_t i = 0;
 
-//Using the autoincrement I must copy the structure of the registers: {X, -- , Y, --, Z}
-int8_t axel_out_data[5] = {0,0,0,0,0};
-
-float coor[NUM_COOR] = {0, 0, 0};
-
-/*For analog string*/
-char string1[STR_LEN] = "\n";
-int length1 = 0;
-
-/*for digital string*/
-char string2[STR_LEN] = "\n";
-int length2 = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -84,8 +95,9 @@ void SystemClock_Config(void);
 static void MX_GPIO_Init(void);
 static void MX_DMA_Init(void);
 static void MX_USART2_UART_Init(void);
-static void MX_I2C1_Init(void);
+static void MX_SPI1_Init(void);
 static void MX_TIM2_Init(void);
+static void MX_TIM3_Init(void);
 /* USER CODE BEGIN PFP */
 
 /* USER CODE END PFP */
@@ -93,30 +105,50 @@ static void MX_TIM2_Init(void);
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
 
-/*Every second (TIM2)*/
-void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim){
-	if(htim==&htim2){
+/*Timers ISR*/
+void HAL_TIM_PeriodElapsedCallback (TIM_HandleTypeDef *htim) {
 
-		/*receive the 3 axis axel digital data in multiple
-		 * read mode (slave_add + 1 to read (READ=1 WRITE=0))*/
-		HAL_I2C_Master_Receive_DMA(&hi2c1, slave_add1+1,  axel_out_data, sizeof(axel_out_data));
+	/*Every 4 ms*/
+	if(htim == &htim2){
 
-		/*Converting in analag values (g)*/
-		//64 is the measured value for 1 g applied on the board (measured using string2)
-		coor[0]= axel_out_data[0]/64.0;
-		coor[1]= axel_out_data[2]/64.0;
-		coor[2]= axel_out_data[4]/64.0;
+		/*When the last column is reached, restart
+		 * (index rollback)*/
+		if (i == MATRIX_ROW){
+			i = 0;
+		}
 
-		length1 = snprintf(string1, STR_LEN, "X:%.3f, Y:%.3f, Z:%.3f\n", coor[0],coor[1],coor[2]);
+		/*TAKE CARE! In data I have to set the pointer as a parameter*/
+		/*Here I transmit a couple, which is stored in the shift register, but
+		 * it is still not reported to the output (no mention of the RCLK)*/
+		HAL_SPI_Transmit_DMA(&hspi1, &tx_char[letter_index][i], MATRIX_COL);
+		i++;
+	}
 
-		//length2 = snprintf(string2, STR_LEN, "X dig:%d, Y dig:%d, Z dig:%d\n", axel_out_data[0],axel_out_data[2],axel_out_data[4]);
+	/*Every second*/
+	if (htim == &htim3){
+		letter_index++;
+		/*Letter index rollback*/
+		if(letter_index == NUM_OF_CHAR){
+			letter_index = 0;
+		}
 
-
-		HAL_UART_Transmit_DMA(&huart2, string1, length1);
-		//HAL_UART_Transmit_DMA(&huart2, string2, length2);
 
 	}
 }
+
+/*When the transmission of a couple of values in completed, I have turn on
+ * RCLK, in order to send them to the output registers, and thus to the led matrix.
+ * This parallel output is triggered by a positive edge so, after set the pin,
+ * I immediately reset it*/
+void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef * hspi){
+	if (hspi == &hspi1){
+		HAL_GPIO_WritePin(RCLK, GPIO_PIN_SET);
+		HAL_GPIO_WritePin(RCLK, GPIO_PIN_RESET);
+	}
+
+}
+
+
 /* USER CODE END 0 */
 
 /**
@@ -150,21 +182,18 @@ int main(void)
   MX_GPIO_Init();
   MX_DMA_Init();
   MX_USART2_UART_Init();
-  MX_I2C1_Init();
+  MX_SPI1_Init();
   MX_TIM2_Init();
+  MX_TIM3_Init();
   /* USER CODE BEGIN 2 */
 
-  //set CTRL_REG1 (+0 to write)
-   HAL_I2C_Master_Transmit(&hi2c1, slave_add1 + 0, ctrl_reg1, sizeof(ctrl_reg1), 10);
+  /*reset of previous possible interrupt requests*/
+  __HAL_TIM_CLEAR_IT(&htim2, TIM_IT_UPDATE);
+  __HAL_TIM_CLEAR_IT(&htim3, TIM_IT_UPDATE);
 
-   /*set sub-address to read axel data: it starts from the x-axis register, which have been set
-    * with MSB=1 in order to enable the autoincrement*/
-   HAL_I2C_Master_Transmit(&hi2c1, slave_add1, &reg_add_x, sizeof(reg_add_x), 10);
-
-  __HAL_TIM_SET_COUNTER(&htim2, 0);
-  __HAL_TIM_CLEAR_IT(&htim2, TIM_IT_UPDATE);	// clear interrupt request BEFORE enabling tim interrupt
+  /*Starting the timers*/
   HAL_TIM_Base_Start_IT(&htim2);
-
+  HAL_TIM_Base_Start_IT(&htim3);
 
   /* USER CODE END 2 */
 
@@ -226,36 +255,40 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief I2C1 Initialization Function
+  * @brief SPI1 Initialization Function
   * @param None
   * @retval None
   */
-static void MX_I2C1_Init(void)
+static void MX_SPI1_Init(void)
 {
 
-  /* USER CODE BEGIN I2C1_Init 0 */
+  /* USER CODE BEGIN SPI1_Init 0 */
 
-  /* USER CODE END I2C1_Init 0 */
+  /* USER CODE END SPI1_Init 0 */
 
-  /* USER CODE BEGIN I2C1_Init 1 */
+  /* USER CODE BEGIN SPI1_Init 1 */
 
-  /* USER CODE END I2C1_Init 1 */
-  hi2c1.Instance = I2C1;
-  hi2c1.Init.ClockSpeed = 100000;
-  hi2c1.Init.DutyCycle = I2C_DUTYCYCLE_2;
-  hi2c1.Init.OwnAddress1 = 0;
-  hi2c1.Init.AddressingMode = I2C_ADDRESSINGMODE_7BIT;
-  hi2c1.Init.DualAddressMode = I2C_DUALADDRESS_DISABLE;
-  hi2c1.Init.OwnAddress2 = 0;
-  hi2c1.Init.GeneralCallMode = I2C_GENERALCALL_DISABLE;
-  hi2c1.Init.NoStretchMode = I2C_NOSTRETCH_DISABLE;
-  if (HAL_I2C_Init(&hi2c1) != HAL_OK)
+  /* USER CODE END SPI1_Init 1 */
+  /* SPI1 parameter configuration*/
+  hspi1.Instance = SPI1;
+  hspi1.Init.Mode = SPI_MODE_MASTER;
+  hspi1.Init.Direction = SPI_DIRECTION_2LINES;
+  hspi1.Init.DataSize = SPI_DATASIZE_8BIT;
+  hspi1.Init.CLKPolarity = SPI_POLARITY_LOW;
+  hspi1.Init.CLKPhase = SPI_PHASE_1EDGE;
+  hspi1.Init.NSS = SPI_NSS_SOFT;
+  hspi1.Init.BaudRatePrescaler = SPI_BAUDRATEPRESCALER_4;
+  hspi1.Init.FirstBit = SPI_FIRSTBIT_MSB;
+  hspi1.Init.TIMode = SPI_TIMODE_DISABLE;
+  hspi1.Init.CRCCalculation = SPI_CRCCALCULATION_DISABLE;
+  hspi1.Init.CRCPolynomial = 10;
+  if (HAL_SPI_Init(&hspi1) != HAL_OK)
   {
     Error_Handler();
   }
-  /* USER CODE BEGIN I2C1_Init 2 */
+  /* USER CODE BEGIN SPI1_Init 2 */
 
-  /* USER CODE END I2C1_Init 2 */
+  /* USER CODE END SPI1_Init 2 */
 
 }
 
@@ -280,7 +313,7 @@ static void MX_TIM2_Init(void)
   htim2.Instance = TIM2;
   htim2.Init.Prescaler = 8400-1;
   htim2.Init.CounterMode = TIM_COUNTERMODE_UP;
-  htim2.Init.Period = 10000-1;
+  htim2.Init.Period = 40-1;
   htim2.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
   htim2.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
   if (HAL_TIM_Base_Init(&htim2) != HAL_OK)
@@ -301,6 +334,51 @@ static void MX_TIM2_Init(void)
   /* USER CODE BEGIN TIM2_Init 2 */
 
   /* USER CODE END TIM2_Init 2 */
+
+}
+
+/**
+  * @brief TIM3 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_TIM3_Init(void)
+{
+
+  /* USER CODE BEGIN TIM3_Init 0 */
+
+  /* USER CODE END TIM3_Init 0 */
+
+  TIM_ClockConfigTypeDef sClockSourceConfig = {0};
+  TIM_MasterConfigTypeDef sMasterConfig = {0};
+
+  /* USER CODE BEGIN TIM3_Init 1 */
+
+  /* USER CODE END TIM3_Init 1 */
+  htim3.Instance = TIM3;
+  htim3.Init.Prescaler = 8400-1;
+  htim3.Init.CounterMode = TIM_COUNTERMODE_UP;
+  htim3.Init.Period = 10000-1;
+  htim3.Init.ClockDivision = TIM_CLOCKDIVISION_DIV1;
+  htim3.Init.AutoReloadPreload = TIM_AUTORELOAD_PRELOAD_DISABLE;
+  if (HAL_TIM_Base_Init(&htim3) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sClockSourceConfig.ClockSource = TIM_CLOCKSOURCE_INTERNAL;
+  if (HAL_TIM_ConfigClockSource(&htim3, &sClockSourceConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  sMasterConfig.MasterOutputTrigger = TIM_TRGO_RESET;
+  sMasterConfig.MasterSlaveMode = TIM_MASTERSLAVEMODE_DISABLE;
+  if (HAL_TIMEx_MasterConfigSynchronization(&htim3, &sMasterConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN TIM3_Init 2 */
+
+  /* USER CODE END TIM3_Init 2 */
 
 }
 
@@ -344,15 +422,12 @@ static void MX_DMA_Init(void)
 {
 
   /* DMA controller clock enable */
-  __HAL_RCC_DMA1_CLK_ENABLE();
+  __HAL_RCC_DMA2_CLK_ENABLE();
 
   /* DMA interrupt init */
-  /* DMA1_Stream0_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream0_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Stream0_IRQn);
-  /* DMA1_Stream6_IRQn interrupt configuration */
-  HAL_NVIC_SetPriority(DMA1_Stream6_IRQn, 0, 0);
-  HAL_NVIC_EnableIRQ(DMA1_Stream6_IRQn);
+  /* DMA2_Stream3_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA2_Stream3_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA2_Stream3_IRQn);
 
 }
 
@@ -374,7 +449,7 @@ static void MX_GPIO_Init(void)
   __HAL_RCC_GPIOB_CLK_ENABLE();
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(LD2_GPIO_Port, LD2_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_6, GPIO_PIN_RESET);
 
   /*Configure GPIO pin : B1_Pin */
   GPIO_InitStruct.Pin = B1_Pin;
@@ -382,12 +457,12 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   HAL_GPIO_Init(B1_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pin : LD2_Pin */
-  GPIO_InitStruct.Pin = LD2_Pin;
+  /*Configure GPIO pin : PB6 */
+  GPIO_InitStruct.Pin = GPIO_PIN_6;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
-  HAL_GPIO_Init(LD2_GPIO_Port, &GPIO_InitStruct);
+  HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
 /* USER CODE BEGIN MX_GPIO_Init_2 */
 /* USER CODE END MX_GPIO_Init_2 */
---
+++ Homework-9/README.md
@@ -0,0 +1,3 @@
+# Homework 9 – LED Matrix Control via SPI and Timer Interrupts
+
+Transmits characters to an LED matrix using SPI in daisy-chain mode with shift registers. A timer interrupt triggers updates, alternating between two letters (or a letter and a symbol).
